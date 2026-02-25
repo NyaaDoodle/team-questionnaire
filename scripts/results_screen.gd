786465803e9369bcece4180cc34877fde8b59247
Melanie Sverdlov <nyaadoodle@gmail.com>
2026-02-25 19:00:20 +0200

    ResultScreen's TitleBackground is no longer visible when TitleLabel's text is empty

--- scripts/results_screen.gd
+++ scripts/results_screen.gd
@@ -3,6 +3,7 @@ class_name ResultsScreen extends GameScreen
 @onready var left_hand_label: Label = $LeftHandLabel
 @onready var right_hand_label: Label = $RightHandLabel
 @onready var title_label: Label = $TitleLabel
+@onready var title_background: ColorRect = $TitleBackground
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("close_results_screen"):
@@ -12,4 +13,5 @@ func setup(left_score: int, right_score: int, background_color: Color, label_tex
 	left_hand_label.text = str(left_score)
 	right_hand_label.text = str(right_score)
 	title_label.text = label_text
+	title_background.visible = not title_label.text.is_empty()
 	change_background_color(background_color)
